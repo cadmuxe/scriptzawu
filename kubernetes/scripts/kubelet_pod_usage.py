014d2be41ed58961c823d27ce20afb03b50695b1
Koonwah Chen <koonwah@google.com>
2020-04-10 14:38:19 -0700

    resource usage pod

--- kubernetes/scripts/kubelet_pod_usage.py
+++ kubernetes/scripts/kubelet_pod_usage.py
@@ -12,32 +12,38 @@ import time
 import argparse
 from datetime import datetime
 import subprocess
+import psutil
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("pods", help="pod names seperate by comma")
-parser.add_argument('--mpstat', action='store_true')
+parser.add_argument('--nodeproc', help="node proc path, this is need to get node resrouce usageinfo.")
+parser.add_argument("--output", default="./", help="output path")
+
 
 args = parser.parse_args()
 pods = args.pods.split(",")
-mpstat = args.mpstat
-default_window = 5
+nodeproc = args.nodeproc
+outputpath = args.output
+default_window = 2
 
 files = {}
-all_file = open("all-usage.txt", "a", buffering=1)
+all_file = open(os.path.join( outputpath,"all-usage.txt"), "a", buffering=1)
 all_header = "Time"
 mpstat_file = None
 
 for p in pods:
-  f = open("%s-usage.txt" % p, "a", buffering=1)
+  f = open( os.path.join( outputpath,"%s-usage.txt" % p), "a", buffering=1)
   files[p] = f
   header = " \t workingSetMBs-{pod} \t usageMBs-{pod} \t usageMillisecondCores-{pod} \t usageCoreMilliseconds-{pod}".format(pod=p)
   out = "Time" + header + " \n"
   all_header += header
   f.write(out)
 
-if mpstat:
-  mpstat_file = open("mpstat-usage.txt", "a", buffering=1)
-  header = " \t %usr \t %sys \t %idle"
+if nodeproc:
+  psutil.PROCFS_PATH = nodeproc
+  mpstat_file = open(os.path.join( outputpath, "mpstat-usage.txt"), "a", buffering=1)
+  header = " \t %cpu_total \t mem_used \t mem_buf \t mem_cache"
   out = "Time" + header + " \n"
   all_header += header
   mpstat_file.write(out)
@@ -46,11 +52,10 @@ all_file.write(all_header + " \n")
 
 # blocking and get the cpu usage for [seconds] seconds.
 def get_mpstat(seconds):
-  r=subprocess.Popen(["mpstat", "-P", "ALL", str(seconds), "1"],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  rr=r.communicate()
-  rt=rr[0].strip().split("\n")[-1].split()
-  # usr sys idle 
-  return rt[2], rt[4], rt[-1]
+  cpu_total = psutil.cpu_percent(seconds)
+  memory = psutil.virtual_memory()
+  # cpu_total, mem_used, mem_buf, mem_cache
+  return cpu_total, memory.used/1024/1024, memory.buffers/1024/1024, memory.cached/1024/1024 
 
 while True:
   summary = urllib2.urlopen("http://localhost:10255/stats/summary").read()
@@ -73,12 +78,12 @@ while True:
     all += usage
     files[targetName].write(out)
 
-  if mpstat:
+  if nodeproc:
     node_usage = get_mpstat(default_window)
-    usage = " \t %s \t %s \t %s" %(node_usage)
+    usage = " \t %s \t %s \t %s \t %s" %(node_usage)
     out = str(datetime.now()) + usage + " \n"
     all += usage
     mpstat_file.write(out)
   else:
-    time.sleep(5)
+    time.sleep(default_window)
   all_file.write(all + " \n")
